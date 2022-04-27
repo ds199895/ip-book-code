a3c45a86d4cedc269740a429eb7b95452f16ac50
Yichen Mo <amomorning@gmail.com>
2022-04-28 00:30:50 +0800

    Add max min and abs

---
+++ ch01/python/untitled/min.py
@@ -0,0 +1,22 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+m = gp.Model()
+
+x, y = map(int, input().split())
+
+z = m.addVar()
+
+m.addConstr(x >= z)
+m.addConstr(y >= z)
+m.addConstr(3 >= z)
+
+M = 100
+u = m.addVars(3, vtype=GRB.BINARY)
+m.addConstr(x <= z + M*(1-u[0]))
+m.addConstr(y <= z + M*(1-u[1]))
+m.addConstr(3 <= z + M*(1-u[2]))
+m.addConstr(u[0] + u[1] + u[2] >= 1)
+m.setObjective(z, GRB.MINIMIZE)
+
+m.optimize()
